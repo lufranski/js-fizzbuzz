09b38caaf15f2109777d290ef8112058feb3c300
lufranski <lufranski@gmail.com>
2022-11-09 14:29:48 +0100

    Main Ex pt.1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // FIZZBUZZ
 
 // Scrivere un programma che stampi in console i numeri da 1 a 100
-
+for (let i = 1; i <= 100; i++){
+    console.log('iterazione #' , i);
+}
 // Condizione per cui i multipli di 3 risultino in console come "Fizz"
 
 // Condizione per cui i multipli di 5 risultino in console come "Buzz"
