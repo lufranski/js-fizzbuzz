abd61d69eff665966ed07ebc7054f0713dbe2035
lufranski <lufranski@gmail.com>
2022-11-09 15:15:48 +0100

    Main Ex Fix

--- js/main.js
+++ js/main.js
@@ -3,22 +3,24 @@
 // Scrivere un programma che stampi in console i numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
     
-    if (i % 3 > 0 && i % 5 > 0) {  
-        
-        console.log('Iterazione #' , i);
+    
+    if (i % 3 == 0 && i % 5 == 0) { // Condizione per cui i multipli di 3 E 5 risultino in console come "FizzBuzz"
 
-    } else if (i % 3 == 0) { // Condizione per cui i multipli di 3 risultino in console come "Fizz"
-
-        console.log('Fizz');
+        console.log('FizzBuzz');
 
     } else if (i % 5 == 0) { // Condizione per cui i multipli di 5 risultino in console come "Buzz"
 
         console.log('Buzz');
     
-    } else if (i % 3 == 0 && i % 5 == 0 ) {
+    } else if (i % 3 == 0 ) { // Condizione per cui i multipli di 3 risultino in console come "Fizz"
         
-        console.log('FizzBuzz');
+        console.log('Fizz');
     
-    }   
+    } else { // Condizione standard
+        
+        console.log('Iterazione #' , i);
+    
+    }    
+  
 }
 
