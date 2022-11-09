5821160d7e41a680ce478e6324d5096234a5d501
lufranski <lufranski@gmail.com>
2022-11-09 14:22:14 +0100

    JS Comments

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// FIZZBUZZ
+
+// Scrivere un programma che stampi in console i numeri da 1 a 100
+
+// Condizione per cui i multipli di 3 risultino in console come "Fizz"
+
+// Condizione per cui i multipli di 5 risultino in console come "Buzz"
+
+// Condizione per cui i multipli di 3 e 5 risultino in console come "FizzBuzz"
