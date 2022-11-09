9b8c4112f124e0558f775d457f692f0c285cd138
lufranski <lufranski@gmail.com>
2022-11-09 14:56:23 +0100

    Main Ex pt.3

--- js/main.js
+++ js/main.js
@@ -11,7 +11,10 @@ for (let i = 1; i <= 100; i++){
 
         console.log('Fizz');
 
-    }
+    } else if (i % 5 == 0) { // Condizione per cui i multipli di 5 risultino in console come "Buzz"
 
+        console.log('Buzz');
+    
+    }    
 }
 
